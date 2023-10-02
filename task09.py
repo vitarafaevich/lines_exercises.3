5a91c5aed9e6f0861bfd4164e9231eea1f66cc7e
vitarafavich <v.rafaevich@g.nsu.ru>
2023-10-02 15:33:31 +0700

    Initial commit

--- task09.py
+++ task09.py
@@ -1,4 +1,11 @@
-'''
+"""
 Задание 9.
 Дано предложение. В нем только два слова одинаковые. Найти эти слова.
-'''
+"""
+
+txt = input()
+text = txt.split()
+for i in text:
+    if text.count(i) == 2:
+        print(i)
+        break
