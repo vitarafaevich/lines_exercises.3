309b6518bcd0a1b059cfdde93972e2c6097a8fb6
vitarafavich <v.rafaevich@g.nsu.ru>
2023-10-03 09:53:27 +0700

    Initial commit

--- task12.py
+++ task12.py
@@ -8,11 +8,15 @@
 
 import keyword
 num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+lttr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+smb = "_"
 txt = input()
 text = txt.split()
 
 if txt[0].isalpha() and txt != keyword:
     for i in range(len(text)):
-        if i.isalpha() or i in num or i == "_":
+        if txt.isalpha():
+            if i in num or i in lttr or i in smb:
             print("подходит")
+        else:
+            print("неподходит")
--- task13.py
+++ task13.py
@@ -8,6 +8,23 @@
 Программа должна вывести его порядковый номер. Счет начинается с 1.
 """
 
-tckts = []
+
+def tckt(ticket):
+    if len(ticket) % 2 == 0:
+        first_p = ticket[:len(ticket)//2]
+        sm_first = sum([int(digit) for digit in first_p])
+        second_p = ticket[len(ticket) // 2:]
+        sm_second = sum([int(digit) for digit in second_p])
+        return sm_first == sm_second
+
+
+tickets = []
 while True:
     ticket = input()
+    tickets.append(ticket)
+    if tckt(ticket):
+        break
+
+for i, ticket in enumerate(tickets, start=1):
+    if tckt(ticket):
+        print(i)
